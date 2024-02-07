0d8dd2b85795ad56cb25abc7f9db9c4abe96b3e9
Lin Yuan <2736414y@student.gla.ac.uk>
2024-02-07 15:52:33 +0000

    git add* git commit -m "Chapter6 Done 16/17" git push

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tango_with_django_project
 
-# python manage.py test rango.tests_chapter5
+# python manage.py test rango.tests_chapter
+# python manage.py makemigrations
+# python manage.py migrate
--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -8,53 +8,43 @@ from rango.models import Category, Page
 
 def populate():
 
-    # First, we will creatne lists of dictionaries containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories.
-    # This might seem a little bit confusing, but it allows us to iterate
-    # through each data structure, and add the data to our models.
-
     python_pages = [
-		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
-		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
-	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
-    
+        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 32},
+        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/", "views": 16},
+        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 8}]
+
     django_pages = [
-		{"title":"Official Django Tutorial",
-	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
-		{"title":"Django Rocks",
-	        "url":"http://www.djangorocks.com/" , "views": 16},
-		{ "title":"How to Tango with Django",
-	        "url":"http://www.tangowithdjango.com/", "views": 8 } ]
-    
+        {"title": "Official Django Tutorial",
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
+        {"title": "Django Rocks",
+         "url": "http://www.djangorocks.com/", "views": 16},
+        {"title": "How to Tango with Django",
+         "url": "http://www.tangowithdjango.com/", "views": 8}]
+
     other_pages = [
-		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
-		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
-    
+        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 32},
+        {"title": "Flask", "url": "http://flask.pocoo.org", "views": 16}]
+
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-			"Django": {"pages": django_pages, "views": 64, "likes": 32},
-        	"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
             "Pascal": {"pages": [], "views": 32, "likes": 16},
             "Perl": {"pages": [], "views": 32, "likes": 16},
             "Php": {"pages": [], "views": 32, "likes": 16},
             "Prolog": {"pages": [], "views": 32, "likes": 16},
             "Programming": {"pages": [], "views": 32, "likes": 16},
-
             }
-    
-    # If you want to add more categories or pages,
-    # Add them to the dictionaries above
-
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            # Ensure view count is greater than zero
+            if p['views'] <= 0:
+                p['views'] = 1  # Set view count to 1 if it's negative or zero
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
-        for p in Page.objects.filter(category = c):
+        for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
     return
